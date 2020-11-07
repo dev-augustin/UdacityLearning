b67940e03246f35691f4b2a74c721439387df51e
dev-augustin <dv.augustin00@gmail.com>
2020-11-06 19:56:30 -0600

    code with update functionality

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ class Todo(db.Model):
   __tablename__ = 'todos'
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False)
+  completed = db.Column(db.Boolean, nullable=False, default=False)
 
   def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
@@ -65,6 +66,21 @@ def create_todo():
         # return jsonify({
         #     'description': todo.description
         # })
+
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+  try:
+    completed = request.get_json()['completed']
+    print('completed', completed)
+    todo = Todo.query.get(todo_id)
+    todo.completed = completed
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return redirect(url_for('index'))
 
 @app.route('/')
 def index():
